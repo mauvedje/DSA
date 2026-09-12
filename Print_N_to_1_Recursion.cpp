e41d234b2be2573b99f6bb93665d9cb52b6b72b2
Mohit <jmohit1107@gmail.com>
2026-09-12 16:36:25 +0530

    i i Intial Commit :wq
    Initial Commit

---
+++ Print_N_to_1_Recursion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void recurse(int n){
+
+    if(n == 0){
+        return;
+    }
+    
+    cout << n << " ";
+    recurse(n-1);
+
+}
+
+int main(){
+
+    int n;
+    cout << "Enter no. of times to print number: ";
+    cin >> n;
+
+    recurse(n);
+
+    return 0;
+}
