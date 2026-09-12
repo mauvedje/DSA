af6b7307150e15f9250e536059f05e6a968e4ef1
Mohit <jmohit1107@gmail.com>
2026-09-12 17:34:53 +0530

    Fixed print statement

--- Print-1-to-N-Backtracking.cpp
+++ Print-1-to-N-Backtracking.cpp
@@ -15,7 +15,7 @@ void recurse(int i, int n){
 int main(){
 
     int n;
-    cout << "Enter no. of times to print number: ";
+    cout << "Enter the number to be printed from 1 to N in backtracking fashion: ";
     cin >> n;
 
     recurse(n, n);
